3b7f4debb3ef03d53bf3941127d6adc5c74e5e3a
stuti-ui <66028064+stuti-ui@users.noreply.github.com>
2021-03-08 17:10:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ import postRoutes from './routes/posts.js';
 
 const app=express()
 
-app.use('/posts',postRoutes);
+
 
 app.use(bodyParser.json({limit:'30mb',extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
 app.use(cors());
 
-const CONNECTION_URL='mongodb+srv://stutisaini_098:qwerty12345@cluster0.2mca9.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.use('/posts',postRoutes);
+
+const CONNECTION_URL='mongodb+srv://stutisaini_098:qwerty12345@cluster0.2mca9.mongodb.net/Cluster0?retryWrites=true&w=majority';
 const PORT=process.env.PORT||5000;
 
-mongoose.connect(CONNECTION_URL,{userNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=> app.listen(PORT,()=>console.log(`Server running on PORT : ${PORT}`)))
     .catch((error)=>console.log(error.message) );
 
